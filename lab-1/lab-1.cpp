41c49d6cf13502e778989f099ca64b9df6535b8c
at-grigorievich <at.grigorievich@gmail.com>
2025-10-18 00:52:24 +0300

    add second question

--- lab-1/lab-1.cpp
+++ lab-1/lab-1.cpp
@@ -6,11 +6,14 @@
 #include <windows.h>
 
 void temperatureExample();
+void manipulateString();
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    temperatureExample();
+    //temperatureExample();
+    manipulateString();
+    
 }
 
 void temperatureExample() {
@@ -53,8 +56,43 @@ void temperatureExample() {
         lowerAverageDays++;
     }
 
+    delete[] temp;
+
     std::cout << "Средняя температура: " << average << std::endl;
     std::cout << "Минимальная температура: " << minTemp << std::endl;
     std::cout << "Максимальная температура: " << maxTemp << std::endl;
     std::cout << "Количество дней с температурой ниже средней: " << lowerAverageDays << std::endl;
+}
+void manipulateString() {
+    char output[256];
+    char findedSymbol = '\0';
+    char endSym = '\0';
+    int length = 0;
+    int findedSymbolCount = 0;
+
+    std::cout << "Введите строку: ";
+    std::cin.getline(output, 256);
+
+    std::cout << "Введите символ для поиска: ";
+    std::cin >> findedSymbol;
+
+    while (output[length] != '\0') {
+        length++;
+    }
+
+    std::cout << "Длина строки: " << length << std::endl;
+
+    for (int i = 0; i < length / 2; i++) {
+        char buf = output[i];
+        output[i] = output[length - i - 1];
+        output[length - i - 1] = buf;
+    }
+    std::cout << "Перевернутая строка: " << output << std::endl;
+
+    for (int i = 0; i < length; i++) {
+        if (output[i] == findedSymbol)
+            findedSymbolCount++;
+    }
+
+    std::cout << "Количество вхождений символа " << findedSymbol << " : " << findedSymbolCount << std::endl;
 }
