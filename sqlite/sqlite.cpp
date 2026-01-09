e5474956d44268f0b9a9b6a018cae0fa81377acd
at-grigorievich <at.grigorievich@gmail.com>
2026-01-09 14:08:51 +0300

    add sqlite3 into project

---
+++ sqlite/sqlite.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <sqlite3.h>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	sqlite3* db;
+	sqlite3_open(":memory:", &db);
+
+	std::cout << "Database opened successfully." << std::endl;
+
+	sqlite3_close(db);
+
+}
